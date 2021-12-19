00c80cacf7c5c318670dd94a92cffa13d11b4b3e
Nguyễn Huy Hoàng <19521539@gm.uit.edu.vn>
2021-12-19 13:36:37 +0700

    fetch feature

--- src/screens/main/ProductDetail/components/features/component/FeaturesText.jsx
+++ src/screens/main/ProductDetail/components/features/component/FeaturesText.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import { View, Text } from 'react-native'
 
-const FeaturesText=({name, content})=>{
+const FeaturesText = ({ name, content }) => {
 
-    return(
+    return (
         <View>
-            <Text style={{fontSize:15,fontWeight:'bold',marginBottom:20}}>{name}</Text>
-            <Text style={{marginBottom:20}}>{content}</Text>
+            <Text style={{ fontSize: 15, fontWeight: 'bold', marginBottom: 20 }}>{name}</Text>
+            <Text style={{ marginBottom: 20 }}>{content}</Text>
         </View>
     )
 }
 
-export default FeaturesText
+export default FeaturesText;
