4abfe68638912fe165e00a6db729c62a6fc401fa
BackTung2807 <92655338+BackTung2807@users.noreply.github.com>
2021-12-27 18:32:29 +0700

    update checkout screen

--- src/screens/main/Cart/Checkout.jsx
+++ src/screens/main/Cart/Checkout.jsx
@@ -29,7 +29,53 @@ const Checkout = ({ navigation }) => {
   return (
     <SafeArea>
       <Header5 navigation={navigation} />
-      
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.main}
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
+        >
+          {data && Object.keys(data).map((item, index) => {
+            return (
+              <CheckoutItem width={width * 0.9} key={index} data={data[item]} />
+            )
+          })}
+          <View style={styles.detail_order_wrapper}>
+            <View style={{ ...styles.detail_order, width: width * 0.8 }}>
+              <TextInput
+                multiline={true}
+                numberOfLines={3}
+                style={{ color: "#0ACF83", marginVertical: 5 }}
+                placeholder='Nhập lời nhắn (địa chỉ, dặn dò, ...)'
+                onChangeText={(text) => setMessage(text)}
+                value={message}
+              />
+            </View>
+            <View style={{ ...styles.detail_order, width: width * 0.8 }}>
+              <Text>Tổng:</Text>
+              <Text>{formatterVnd(sum)}</Text>
+            </View>
+            <View style={{ ...styles.detail_order, width: width * 0.8 }}>
+              <Text>Thuế GTTT:</Text>
+              <Text>{formatterVnd(sum * 0.1)}</Text>
+            </View>
+            <View style={{ ...styles.detail_order, width: width * 0.8 }}>
+              <Text>Phí vận chuyển:</Text>
+              <Text>30.000 VND</Text>
+            </View>
+            <View style={{ ...styles.detail_order, width: width * 0.8 }}>
+              <Text style={{ fontWeight: "bold" }}>Thành tiền:</Text>
+              <Text style={{ fontWeight: "bold" }}>{formatterVnd(sum * 1.1 + 30000)}</Text>
+            </View>
+            <View style={{width: width * 0.9, marginVertical: 25}}>
+            <Button
+              title="Đặt hàng"
+              color="#0ACF83"
+            />
+          </View>
+          </View>
+        </ScrollView>
+      </View>
     </SafeArea>
   )
 }
